1def1072cbf5c4100dce52b19b3d9340809dd847
FilipCmral <flints.cz@gmail.com>
2026-05-29 17:12:16 +0200

    feat: základní volání OpenTripMap API

--- src/api/fetchPointsOfInterest.ts
+++ src/api/fetchPointsOfInterest.ts
@@ -1,21 +1,40 @@
-import { type PointOfInterest } from '../types/pointOfInterest.types';
+import { type PointOfInterest } from "../types/pointOfInterest.types";
 
-export async function fetchPointsOfInterest(cityName: string): Promise<PointOfInterest[]> {
-    const defaultLimit = 100; // Default limit for number of points of interest
-    const defaultRadius = 10000; // Default radius in meters
-    const defaultFormat = 'json'; // Default response format
+const apiKey = import.meta.env.VITE_API_KEY;
 
+export async function fetchPointsOfInterest(
+  cityName: string,
+): Promise<PointOfInterest[]> {
+  const defaultLimit = 100; // Limit for number of points of interest
+  const defaultRadius = 10000; // Radius in meters
+  const defaultFormat = "json";
 
-    const response = await fetch(`https://api.opentripmap.com/0.1/en/places/geoname
-        ?name=${encodeURIComponent(cityName)}
-        &radius=${defaultRadius}
+  const fetchCityDetailsResponse = await fetch(
+    `https://api.opentripmap.com/0.1/en/places/geoname?name=${encodeURIComponent(cityName)}
+        &key=${apiKey}`,
+  );
+
+  if (!fetchCityDetailsResponse.ok) {
+    throw new Error(`Failed to fetch city details for city: ${cityName}`);
+  }
+
+  const cityDetails = await fetchCityDetailsResponse.json();
+  const { lattitude, longitude } = cityDetails;
+
+  const pointsOfInterestResponse = await fetch(
+    `https://api.opentripmap.com/0.1/en/places/geoname
+        ?radius=${defaultRadius}
+        &lon=${longitude}
+        &lat=${lattitude}
         &limit=${defaultLimit}
         &format=${defaultFormat}
-        `);
-    if (!response.ok) {
-        throw new Error(`Failed to fetch points of interest for city: ${cityName}`);
-    }
+        &key=${apiKey}`,
+  );
 
-    const data = await response.json();
-    return data as PointOfInterest[];
-}  
+  if (!pointsOfInterestResponse.ok) {
+    throw new Error(`Failed to fetch points of interest for city: ${cityName}`);
+  }
+
+  const pointsOfInterestData = await pointsOfInterestResponse.json();
+  return pointsOfInterestData as PointOfInterest[];
+}
