9127745fa885cc19a02e89f7c9e63f3370763942
FilipCmral <flints.cz@gmail.com>
2026-06-02 21:47:53 +0200

    feat: added favicons to the main page for better visuals

--- src/pages/SearchForPointsOfInterestPage.tsx
+++ src/pages/SearchForPointsOfInterestPage.tsx
@@ -14,7 +14,9 @@ export function SearchForPointsOfInterestPage() {
   return (
     <div className={styles.page}>
       <div className={styles.header}>
-        <h1 className={styles.title}>Open Trip</h1>
+        <h1 className={styles.title}>
+          Open Trip  <svg xmlns="http://www.w3.org/2000/svg" width="64" height="64" fill="currentColor" className="bi bi-geo-alt-fill" viewBox="0 0 16 16"><path d="M8 16s6-5.686 6-10A6 6 0 0 0 2 6c0 4.314 6 10 6 10m0-7a3 3 0 1 1 0-6 3 3 0 0 1 0 6"/></svg>
+        </h1>
         <SearchPointsOfInterestForm onSearch={makeSearch} updateFormTitle={updateFormTitle} />
       </div>
 
